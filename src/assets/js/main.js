30621e60a3d6ddbad4115f007152abd5a0369212
Ramy Anber <ramyanber@gmail.com>
2021-04-05 00:08:12 -0400

    portflio

---
+++ src/assets/js/main.js
@@ -0,0 +1,64 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+
+$(document).ready(function() {
+  AOS.init( {
+    // uncomment below for on-scroll animations to played only once
+    // once: true  
+  }); // initialize animate on scroll library
+});
+
+$('img[data-enlargeable]').addClass('img-enlargeable').click(function(){
+  var src = $(this).attr('src');
+  var modal;
+  function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
+  modal = $('<div>').css({
+      background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+      backgroundSize: 'contain',
+      width:'100%', height:'100%',
+      position:'fixed',
+      zIndex:'10000',
+      top:'0', left:'0',
+      cursor: 'zoom-out'
+  }).click(function(){
+      removeModal();
+  }).appendTo('body');
+  //handling ESC
+  $('body').on('keyup.modal-close', function(e){
+    if(e.key==='Escape'){ removeModal(); } 
+  });
+});
+
+// Smooth scroll for links with hashes
+$('a.smooth-scroll')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
